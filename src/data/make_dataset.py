255e91255de150ecfd617962c83f0fd134aa69b2
fredrichter80 <fred.richter80@gmail.com>
2025-11-19 20:14:07 +0100

    Modify target variable

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -79,8 +79,8 @@ def process_data(
     # Add new columns
     df = add_new_columns(df, nb_victim, nb_vehicules)
 
-    # # Modify target variable
-    # df = modif_target_variable(df)
+    # Modify target variable
+    df = modif_target_variable(df)
 
     # # Replace values -1 and 0
     # df = replace_values(df)
